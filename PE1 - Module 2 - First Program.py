4ec22e4416309946d279e952f215498d78f5a638
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-08-26 14:04:16 -0400

    Update PE1 - Module 2 - First Program.py

--- PE1 - Module 2 - First Program.py
+++ PE1 - Module 2 - First Program.py
@@ -2,23 +2,20 @@
 
 # As we're going to show you some fundamental concepts and terms, these snippets of code won't be serious or complex.
 
-# Run the code in the editor window on the right. If everything goes okay here, you'll see the line of text in the console window.
-
-# Alternatively, launch IDLE, create a new Python source file, fill it with this code, name the file and save it. Now run it. If everything goes okay, you'll see the rhyme's line in the IDLE console window. The code you have run should look familiar. You saw something very similar when we led you through the setting up of the IDLE environment.
-
-# Now we'll spend some time showing and explaining to you what you're actually seeing, and why it looks like this.
+# First we'll spend some time showing and explaining to you what you're actually seeing, and why it looks like this.
 
 # As you can see, the first program consists of the following parts:
 
 # - the word "print";
 # - an opening parenthesis;
-# - a quotation mark;
-# - a string / line of text
-# - another quotation mark;
+# - a opening quotation mark;
+# - a string of text.
+# - a closing quotation mark;
 # - a comma, separating strings.
-# - a keyword argument "end".
+# - the separator / keyword argument "end".
 # - a closing parenthesis.
+
 # Each of the above plays a very important role in the code:
     
-print("The itsy bitsy spider " , end="")
-print("climbed up" , "the waterspout.")
+print("Hello", end=" ")
+print("World!")
