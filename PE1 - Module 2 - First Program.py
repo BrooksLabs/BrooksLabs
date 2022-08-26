177af2d01baaa49eef2106207b419b089860d742
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-08-26 13:12:06 -0400

    Update PE1 - Module 2 - First Program.py

--- PE1 - Module 2 - First Program.py
+++ PE1 - Module 2 - First Program.py
@@ -10,12 +10,12 @@
 
 # As you can see, the first program consists of the following parts:
 
-# the word print;
-# an opening parenthesis;
-# a quotation mark;
-# a line of text: Hello, World!;
-# another quotation mark;
-# a closing parenthesis.
+# - the word print;
+# - an opening parenthesis;
+# - a quotation mark;
+# - a line of text: Hello, World!;
+# - another quotation mark;
+# - a closing parenthesis.
 # Each of the above plays a very important role in the code:
     
 print("Hello, World!")
