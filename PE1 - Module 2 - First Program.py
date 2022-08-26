f40ecbe5d8ebf63c23e81c072ce3eb19d6ba422f
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-08-26 17:37:01 -0400

    Update PE1 - Module 2 - First Program.py

--- PE1 - Module 2 - First Program.py
+++ PE1 - Module 2 - First Program.py
@@ -1,19 +1,13 @@
-# It's time to start writing some real, working Python code. It'll be very simple for the time being.
-
-# As we're going to show you some fundamental concepts and terms, these snippets of code won't be serious or complex.
-
-# First we'll spend some time showing and explaining to you what you're actually seeing, and why it looks like this.
-
-# As you can see, the first program consists of the following parts:
-
-# - the word "print";
-# - an opening parenthesis;
-# - a opening quotation mark;
-# - a string of text.
-# - a closing quotation mark;
-# - a comma, separating strings.
-# - the separator / keyword argument "end".
-# - a closing parenthesis.
+#As you can see, the first program consists of the following parts:
+    
+# - The word "print";
+# - An opening parenthesis;
+# - A opening quotation mark;
+# - A string of text.
+# - A closing quotation mark;
+# - A comma, separating strings.
+# - The separator / keyword argument "end".
+# - A closing parenthesis.
 
 # Each of the above plays a very important role in the code:
     
