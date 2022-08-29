e2ec7a49bf5da69205de7da5077f8d6adbb28a3f
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-08-29 10:55:20 -0400

    Add files via upload

---
+++ PE1 - Module 2 - The input() function.py
@@ -0,0 +1,12 @@
+print("What is today's date?") # print() function
+date = input() # input() function
+day = input("What day of the week is it? ") # This method merges the print() and input() into one line.
+print("Today is", day, "~", date)
+
+#   The input() function is a mirror reflection of the print() function; print() sends data to the console, input() gets data from it.
+#   The input() function is able to read data entered by the user and to return the same data to the running program.
+#   Virtually all programs read and process data. A program which doesn't get a user's input is a deaf program.
+#   The program will prompt the user to input some data from the console (most likely using a keyboard, although it is also possible to input data using voice or image).
+
+# NOTE:
+#   The result must be assigned to a variable or else the entered data will be lost.
