8e92b2623460deb229aa8acf31625273c9a7db06
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-08-30 12:38:38 -0400

    Update PE1 - Module 2 - The input() function.py

--- PE1 - Module 2 - The input() function.py
+++ PE1 - Module 2 - The input() function.py
@@ -1,7 +1,7 @@
 print("What is today's date?") # print() function
 date = input() # input() function
 day = input("What day of the week is it? ") # This method merges the print() and input() into one line.
-print("Today is", day, "~", date)
+print("Today is", day, "~ " + date)
 
 #   The input() function is a mirror reflection of the print() function; print() sends data to the console, input() gets data from it.
 #   The input() function is able to read data entered by the user and to return the same data to the running program.
@@ -12,6 +12,17 @@ print("Today is", day, "~", date)
 #   The int() function takes one argument (e.g., a string: int(string)) and tries to convert it into an integer; if it fails, the whole program will fail too.
 #   The float() function takes one argument (e.g., a string: float(string)) and tries to convert it into a float (the rest is the same).
 
+# CONCATENATION:
+#   The + (plus) sign, when applied to two strings, becomes a concatenation operator: string + string
+
+# REPLICATION:
+#   The * (asterisk) sign, when applied to a string and number (or a number and string, as it remains commutative in this position) becomes a replication operator.
+
+print("This lesson is on replication:")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
+
 # NOTES:
 #   The result must be assigned to a variable or else the entered data will be lost.
 #   The result of the input() function is also a string.
+#   In contrast to its arithmetic sibling, the concatenation operator is not commutative, i.e., "ab" + "ba" is not the same as "ba" + "ab".
