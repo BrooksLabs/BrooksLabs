434ff527cc9b988281bf15198ba15a7a1a865d42
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-09-02 11:27:15 -0400

    Update PE1 - Module 3 - While Loops.py

--- PE1 - Module 3 - While Loops.py
+++ PE1 - Module 3 - While Loops.py
@@ -1,6 +1,7 @@
 # THE WHILE LOOP:
 #   With the while loop we can execute a set of statements as long as a condition is true.
 
+print("Example #1:")
 i = 1
 while i < 6:
   print(i) # Print i as long as i is less than 6.
@@ -9,6 +10,7 @@ while i < 6:
 # THE BREAK STATEMENT:
 #   With the break statement we can stop the loop even if the while condition is true:
 
+print("Example #2:")
 i = 1
 while i < 6:
   print(i)
@@ -19,6 +21,7 @@ while i < 6:
 # THE CONTINUE STATEMENT:
 #   With the continue statement we can stop the current iteration, and continue with the next:
 
+print("Example #3:")
 i = 0
 while i < 6:
   i += 1
@@ -29,7 +32,8 @@ while i < 6:
 # THE ELSE STATEMENT:
 #   With the else statement we can run a block of code once when the condition no longer is true:
 
-    i = 1
+print("Example #4:")
+i = 1
 while i < 6:
   print(i)
   i += 1
@@ -38,7 +42,9 @@ else:
   
 # THE INFINITE LOOP:
 #   An endless loop, is a sequence of instructions in a program which repeat indefinitely (loop endlessly.)
+#   An infinite loop only stops with external intervention or when a break statement is found. You can stop an infinite loop with CTRL + C.
 
+print("Example #5:")
 while True:
     print("I'm stuck inside a loop.") # This loop cannot complete execution and will infinitely print "I'm stuck inside a loop." on the screen.
-
+    break
