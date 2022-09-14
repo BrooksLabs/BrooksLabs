e000c82626e5a345845f79431cae7191d37a113a
Brooks Labs™ <102487156+BrooksLabs@users.noreply.github.com>
2022-09-14 11:04:45 -0400

    Update PE1 - Module 3 - Lists.py

--- PE1 - Module 3 - Lists.py
+++ PE1 - Module 3 - Lists.py
@@ -22,20 +22,20 @@ print("\nCurrent List:", numbers)  # Printing current list content.
 # ACCESSING LIST CONTENT:
 #   Each of the list's elements may be accessed separately.
 
-print("\nThe first element in this list is:", numbers[0]) # Accessing the list's first element.
+print("The first element in this list is:", numbers[0]) # Accessing the list's first element.
 
 # THE len() FUNCTION:
 #   This function is used to check the list's current length.
 #   len() uses the list's name as an argument, and returns the number of elements currently stored inside the list.
 
-print("List length:", len(numbers))  # Printing the list's length.
+print("List length:", len(numbers), "elements")  # Printing the list's length.
 
 # THE del() INSTRUCTION / REMOVING ELEMENTS FROM A LIST:
 #   Any of the list's elements may be removed at any time with an instruction named del (delete).
 
 del numbers[1]  # Removing the second element from the list.
-print("\nNew list content:", numbers)  # Printing current list content.
-print("New list's length:", len(numbers))  # Printing new list length.
+print("\nNew List:", numbers)  # Printing current list content.
+print("New List length:", len(numbers), "elements") # Printing new list length.
 
 # NEGATIVE INDICIES:
 #   Negative indices are legal; An element with an index equal to -1 is the last one in the list.
@@ -43,7 +43,39 @@ print("New list's length:", len(numbers))  # Printing new list length.
 #   The last accessible element in our list is numbers[-4] (the first one) - don't try to go any further!
 
 numbers = [-1, 2, 3, 4]
-print(numbers[-1])
-print(numbers[-2])
+print("The last value in this list is:", numbers[-1])
+print("The second-to-last value in this list is:", numbers[-2])
 
+# LIST METHODS:
+#   - A method is a specific kind of function - it behaves like a function and looks like a function, but differs in the way in which it acts, and in its invocation style.
+#   - A function doesn't belong to any data - it gets data, it may create new data and it (generally) produces a result.
+#   - A method does all these things, but is also able to change the state of a selected entity.
+#   - A method is owned by the data it works for, while a function is owned by the whole code.
+
+# ADDING ELEMENTS TO A LIST: append() AND insert()
+
+numbers = [1, 2, 3, 4]
+print("")
+print(numbers)
+print("This list has", len(numbers), "elements")
+
+numbers.append(5) # Append 5 to the end of the list.
+print("")
+print(numbers)
+print("The appended list has", len(numbers), "elements")
+
+numbers.insert(0, -1) # Insert -1 as the first element.
+print("")
+print(numbers)
+print("This list has", len(numbers), "elements")
+
+# CREATING AN EMPTY LIST:
     
+my_list = [] #creating an empty list
+
+for i in range(5):
+    my_list.append(i + 1) # creates a list of 4 elements that increases by 1 using the for loop.
+
+print("")
+print(my_list)
+print("This list has", len(my_list), "elements")
